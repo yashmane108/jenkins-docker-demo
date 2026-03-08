d1b9ef5c56847d8dc7b935ee6debc617ab64040c
RAJARAM MANE <yashmane9872@gmail.com>
2026-03-08 12:32:44 +0530

    edit2 app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Jenkins CI/CD Pipeline. Now add Github weebhook"
+    return "Hello from Jenkins CI/CD Pipeline. github webhook"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
