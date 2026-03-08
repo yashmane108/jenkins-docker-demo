f9f0e7bdcb27a7985657ad8a35462011e6bdc5d2
RAJARAM MANE <yashmane9872@gmail.com>
2026-03-08 12:45:37 +0530

    edit3 from local app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Jenkins CI/CD Pipeline. github webhook. test 2"
+    return "Hello from Jenkins CI/CD Pipeline. github webhook. test 2. " \
+    "changes from local and then push"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
